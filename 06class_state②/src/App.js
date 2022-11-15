a5d0870e5f42b53fa71fe84478edd88beb9baf2e
Chimoha31 <mokochii1108@gmail.com>
2022-11-15 09:39:45 -0800

    Done

--- 06class_state②/src/App.js
+++ 06class_state②/src/App.js
@@ -1,11 +1,61 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./App.css";
 
-const App = () => {
-  return (
-    <div>
-      App
-    </div>
-  )
+// -------------------------
+// Functional Component
+// -------------------------
+// const App = () => {
+//   const [goOut, setGoOut] = useState("Yes");
+
+//   const handleToggle = () => {
+//     setGoOut((prev) => {
+//       return prev === "Yes" ? "No" : "Yes";
+//     });
+//   };
+
+//   return (
+//     <div>
+//       <p>Funtional Component</p>
+//       <h1>Q: Should I go out tonight?</h1>
+//       <button className="button1" onClick={handleToggle}>
+//         {goOut}
+//       </button>
+//       <div>
+//         <h1>A: {goOut}</h1>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default App;
+
+// -------------------------
+// Class Component
+// -------------------------
+export default class App extends React.Component {
+  state = {
+    goOut: "Yes",
+  };
+
+  toggleGoOut() {
+    console.log("Clicked");
+    setGoOut((prev) => {
+      return prev === "Yes" ? "No" : "Yes";
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <p>Class Component</p>
+        <h1>Q: Should I go out tonight?</h1>
+        <button className="button1" onClick={this.toggleGoOut}>
+          {this.state.goOut}
+        </button>
+        <div>
+          <h1>A: {this.state.goOut}</h1>
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App
